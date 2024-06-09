4f223efdef1ad4c9f4330b1bade28652f9f11924
Rafael Pereira <rafael.zdd@gmail.com>
2024-06-09 11:06:04 -0300

    Constrói estrutura do site

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ejs from "ejs";
 
 const app = express();
 const porta = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/quem-somos", (req, res) => {
+  res.render("quem-somos.ejs");
+});
+
+app.get("/unidades", (req, res) => {
+  res.render("unidades.ejs");
+});
+
+app.get("/contato", (req, res) => {
+  res.render("contato.ejs");
+});
+
+app.get("/area-do-cidadao", (req,res) => {
+  res.render("area-do-cidadao.ejs");
+});
 
 app.listen(porta, () => {
   console.log(`Listening on port ${porta}`);
