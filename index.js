f69349cb68bb5eb743ddae348a45022a9c1f7d0a
Rafael Pereira <rafael.zdd@gmail.com>
2024-06-16 13:15:05 -0300

    Adiciona tela de login

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
-
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "puc-si=e5",
-  password: "123456",
-  port: 5432,
-});
-
-db.connect();
-
-db.query("query", (err, res) => {
-  if(err) {
-
-  } else {
-
-  }
-  db.end();
-});
+import ejs from "ejs";
+import pool from "./database.js";
 
 const app = express();
 const porta = 3000;
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
@@ -47,10 +31,50 @@ app.get("/area-do-cidadao", (req,res) => {
   res.render("area-do-cidadao.ejs");
 });
 
+app.get("/cadastro-de-usuario", (req, res) => {
+  res.render("cadastro-de-usuario.ejs");
+});
+
 app.post("/check", (req, res) => {
-  // if(){
-  //   res.render("marcacao-de-consulta.ejs");
-  // };
+  const { username, password } = req.body;
+  if(!username || !password) {
+    res.status(400).send("Username e Password são obrigatórios.");
+    return;
+  }
+
+pool.query(
+    "SELECT * FROM users WHERE username = $1 AND password = $2", [username, password],
+    (err, result) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send("Erro no servidor");
+      } else if (result.rows.length > 0) {
+        res.render("marcacao-de-consulta.ejs");
+      } else {
+        res.send("Usuário ou senha incorretos");
+      }
+    }
+  );
+});
+
+app.post("/criar-usuario", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).send("Username e Password são obrigatórios.");
+  }
+
+  pool.query(
+    "INSERT INTO users (username, password) VALUES ($1, $2)",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).send("Erro ao criar usuário.");
+      }
+      res.send("Usuário criado com sucesso!");
+    }
+  );
 });
 
 app.listen(porta, () => {
