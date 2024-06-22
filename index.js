ff35776d6e7a0b95b90956378584148416a2d7d2
Rafael Pereira <rafael.zdd@gmail.com>
2024-06-22 14:54:53 -0300

    Inclui redirecionamento automático de página nas rotinas de login e cadastro

--- index.js
+++ index.js
@@ -52,28 +52,49 @@ app.post("/check", (req, res) => {
         } else if (result.rows.length > 0) {
           res.render("marcacao-de-consulta.ejs", { titulo: "Marcação de Consulta", year: today.getFullYear() });
         } else {
-          res.send("Usuário ou senha incorretos");
+          res.send(`
+            <p>Usuário ou senha incorretos</p>
+            <script>
+              setTimeout(() => {
+                window.location.href = "/area-do-cidadao";
+              }, 3000);
+            </script>
+          `);
         }
       }
     );
 });
 
 app.post("/criar-usuario", (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, nome, email } = req.body;
 
-  if (!username || !password) {
-    return res.status(400).send("Username e Password são obrigatórios.");
+  if (!username || !password || !nome || !email) {
+    return res.status(400).send("Todos os campos são obrigatórios.");
   }
 
   pool.query(
-    "INSERT INTO users (username, password) VALUES ($1, $2)",
-    [username, password],
+    "INSERT INTO users (username, password, nome, email) VALUES ($1, $2, $3, $4)",
+    [username, password, nome, email],
     (err, result) => {
       if (err) {
         console.error(err.message);
-        return res.status(500).send("Erro ao criar usuário.");
+        return res.status(500).send(`
+        <p>Erro ao efetuar o cadastro.</p>
+        <script>
+          setTimeout(() => {
+            window.location.href = "/area-do-cidadao";
+          }, 3000);
+        </script>
+      `);
       }
-      res.send("Usuário criado com sucesso!");
+      res.send(`
+        <p>Cadastro realizado com sucesso!</p>
+        <script>
+          setTimeout(() => {
+            window.location.href = "/area-do-cidadao";
+          }, 3000);
+        </script>
+      `);  
     }
   );
 });
